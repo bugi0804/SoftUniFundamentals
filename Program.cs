cbd67cc479fe48e4785b7b86470957fca0dc8a67
bugi0804 <134012213+bugi0804@users.noreply.github.com>
2023-05-22 23:44:10 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _01.Basic_Syntax__Conditional_Statements_and_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            double grade = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Name: {name}, Age: {age}, Grade: {grade:f2}.");
+        }
+    }
+}
